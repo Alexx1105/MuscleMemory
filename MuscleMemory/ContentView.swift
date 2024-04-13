0a06f783b40158b13b527fef12196c11a647270b
Alexx1105 <alexh2877@gmail.com>
2024-04-13 15:10:43 -0400

    Initial Commit

---
+++ MuscleMemory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MuscleMemory
+//
+//  Created by alex haidar on 4/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MuscleMemory/MuscleMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuscleMemoryApp.swift
+//  MuscleMemory
+//
+//  Created by alex haidar on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MuscleMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
