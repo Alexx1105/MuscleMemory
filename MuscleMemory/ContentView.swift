6809c5147cb1e5a98c48c743c970e3bc2d35fb87
Alex <132936435+Alexx1105@users.noreply.github.com>
2024-10-21 17:42:04 -0400

    added darkmode and functional auth (#5)
    * removed hex code 32 bit converter
    
    * removed hex code 32 bit converter
    
    * WIP front end for sign in
    
    * [WIP] test current authentication solution
    
    * rehan test this
    
    * dark mode

--- MuscleMemory/ContentView.swift
+++ MuscleMemory/ContentView.swift
@@ -11,36 +11,9 @@ import ActivityKit
 import UIKit
 
 
-extension Color {
-    init(hex: String) {
-        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int: UInt64 = 0
-        Scanner(string: hex).scanHexInt64(&int)
-        let a, r, g, b: UInt64
-        switch hex.count {
-        case 3: // RGB (12-bit)
-            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-        case 6: // RGB (24-bit)
-            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-        case 8: // ARGB (32-bit)
-            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-        default:
-            (a, r, g, b) = (255, 0, 0, 0)
-        }
-        
-        self.init(
-            .sRGB,
-            red: Double(r) / 255,
-            green: Double(g) / 255,
-            blue: Double(b) / 255,
-            opacity: Double(a) / 255
-        )
-    }
-}
-
 
 struct ContentView: View {
-    
+
     
     @StateObject var NotionCaller = NotionCall()   //manage lifecycle of instance
     
@@ -49,8 +22,6 @@ struct ContentView: View {
     @State var menuFunc = String()   //modify to take user back to menu later
     @State var notificationSettings = String() //modify to take user to notifications settings page later
     @State var importNotionFile = String() //modify for user to be able to import their notion file for parsing
-    
-    
     
     
     
@@ -108,8 +79,13 @@ struct ContentView: View {
             NotionCaller.makeAPIRequest()
             
         }
-    }
-}
+    
+            
+                  
+            }
+        }
+    
+
 
 
 #Preview {
---
+++ MuscleMemory/authView.swift
@@ -0,0 +1,99 @@
+//
+//  authView.swift
+//  MuscleMemory
+//
+//  Created by alex haidar on 10/12/24.
+//
+
+import SwiftUI
+import AuthenticationServices
+import Foundation
+
+struct authView: View {
+    
+    @ObservedObject var auth = authBackend()
+    @Environment(\.colorScheme) var colorScheme
+    
+    var body: some View {
+        
+        
+        VStack {
+          
+            Spacer()
+            Image("auth")
+                .renderingMode(.template)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 330, height: 330)
+                .padding(.trailing, 10)
+                .padding(.top, 60)
+                
+          
+            Spacer()
+            Text("Sign into MuscleMemory ")
+                .fontWeight(.bold)
+                .font(.system(size: 16))
+                .padding(.trailing, 70)
+                .padding(.bottom, 1)
+         
+            Text("Powered by Kimchi Labs  ")
+                .fontWeight(.medium)
+                .foregroundStyle(Color.gray)
+                .font(.system(size: 14))
+                .padding(.trailing, 90)
+       
+            Divider()
+                .frame(width: 360, height: 1)
+              
+                .padding(.bottom, 4)
+            
+            Group {
+                switch colorScheme {
+                case .light:
+                    SignInWithAppleButton(.signIn, onRequest: { request in
+                        request.requestedScopes = [.fullName, .email]
+                    }, onCompletion: { result in
+                        switch result {
+                        case .success(let authorization):
+                            auth.handleSuccessfulLogin(authorization)
+                        case .failure(let error):
+                            auth.handleLoginError(with: error)
+                        }
+                    })
+                    .signInWithAppleButtonStyle(.black)
+                case .dark:
+                    SignInWithAppleButton(.signIn, onRequest: { request in
+                        request.requestedScopes = [.fullName, .email]
+                    }, onCompletion: { result in
+                        switch result {
+                        case .success(let authorization):
+                            auth.handleSuccessfulLogin(authorization)
+                        case .failure(let error):
+                            auth.handleLoginError(with: error)
+                        }
+                    })
+                    .signInWithAppleButtonStyle(.white)
+                @unknown default:
+                    EmptyView()
+                }
+            }
+            .frame(width: 297, height:  43)
+            .cornerRadius(20)
+            .padding(.bottom, 130)
+            
+
+            
+        } .frame(maxWidth: .infinity, maxHeight: .infinity)
+        
+        
+        
+        
+        
+    }
+    
+}
+
+#Preview {
+        authView()
+    }
+
