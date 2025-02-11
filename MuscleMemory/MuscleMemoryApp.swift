428a574a67cb86c3131e0c1c967a97b75c8ece60
Alexx1105 <alexh2877@gmail.com>
2025-02-11 09:24:47 -0500

    popover menu for managing DynamicRep settings for Notion page sets

--- MuscleMemory/MuscleMemoryApp.swift
+++ MuscleMemory/MuscleMemoryApp.swift
@@ -9,34 +9,39 @@ import SwiftUI
 import AuthenticationServices
 
 
-struct ContainerView: View {
+
     
     
-    @StateObject var navigationPath = NavPath.shared
     
-    var body: some View {
+    
+    struct ContainerView: View {
         
-        NavigationStack(path: $navigationPath.path) {
-            LaunchScreen()
-                .navigationDestination(for: NavPathItem.self) { navigationPathItem in
-                    switch navigationPathItem {
-                    case .home:
-                        MainMenu()
-                    case .settings:
-                        SettingsView()
-                    case .importPage:
-                        NotionImportPageView()
-                    case .logOut:
-                        SignOutView()
-                    case .appearence:
-                        LightDarkView()
-                    case .importpageUser:
-                        ImportedNotes()
+        
+        @StateObject var navigationPath = NavPath.shared
+        
+        var body: some View {
+            
+            NavigationStack(path: $navigationPath.path) {
+                LaunchScreen()
+                    .navigationDestination(for: NavPathItem.self) { navigationPathItem in
+                        switch navigationPathItem {
+                        case .home:
+                            MainMenu()
+                        case .settings:
+                            SettingsView()
+                        case .importPage:
+                            NotionImportPageView()
+                        case .logOut:
+                            SignOutView()
+                        case .appearence:
+                            LightDarkView()
+                        case .importpageUser:
+                            ImportedNotes()
+                        }
                     }
                 }
+            }
         }
-    }
-}
 
 @main
 struct MuscleMemoryApp: App {
